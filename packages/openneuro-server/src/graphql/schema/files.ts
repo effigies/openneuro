1691316976ecf1ac94547233530a26de5cd286f1
Christopher J. Markiewicz <markiewicz@stanford.edu>
2026-04-14 15:17:40 -0400

    feat(server): Define Pothos Permission, file, and Description types

---
+++ packages/openneuro-server/src/graphql/schema/files.ts
@@ -0,0 +1,48 @@
+import { builder } from "../builder"
+import { Severity } from "./enums"
+
+export const ValidationIssueFileDetail = builder.simpleObject(
+  "ValidationIssueFileDetail",
+  {
+    fields: (t) => ({
+      name: t.string(),
+      path: t.string(),
+      relativePath: t.string(),
+    }),
+  },
+)
+
+export const ValidationIssueFile = builder.simpleObject("ValidationIssueFile", {
+  fields: (t) => ({
+    name: t.string(),
+    path: t.string(),
+    key: t.string({ nullable: false }),
+    code: t.int(),
+    file: t.field({ type: ValidationIssueFileDetail }),
+    evidence: t.string(),
+    line: t.int(),
+    character: t.int(),
+    severity: t.field({ type: Severity, nullable: false }),
+    reason: t.string(),
+    helpUrl: t.string(),
+  }),
+})
+
+export const DatasetFile = builder.simpleObject("DatasetFile", {
+  fields: (t) => ({
+    id: t.id({ nullable: false }),
+    filename: t.string({ nullable: false }),
+    size: t.field({ type: "BigInt" }),
+    annexed: t.boolean(),
+    urls: t.stringList(),
+    directory: t.boolean(),
+  }),
+})
+
+export const FilesUpdate = builder.simpleObject("FilesUpdate", {
+  fields: (t) => ({
+    datasetId: t.string(),
+    action: t.string(),
+    payload: t.field({ type: [DatasetFile] }),
+  }),
+})
---
+++ packages/openneuro-server/src/graphql/schema/permissions.ts
@@ -0,0 +1,34 @@
+import { builder } from "../builder"
+import { UserRef } from "./user"
+
+export const PermissionRef = builder.objectRef<Record<string, unknown>>(
+  "Permission",
+)
+
+PermissionRef.implement({
+  fields: (t) => ({
+    datasetId: t.id({
+      nullable: false,
+      resolve: (obj) => obj.datasetId as string,
+    }),
+    userId: t.string({
+      nullable: false,
+      resolve: (obj) => obj.userId as string,
+    }),
+    level: t.string({
+      nullable: false,
+      resolve: (obj) => obj.level as string,
+    }),
+    user: t.field({
+      type: UserRef,
+      resolve: (obj) => obj.user as Record<string, unknown>,
+    }),
+  }),
+})
+
+export const DatasetPermissions = builder.simpleObject("DatasetPermissions", {
+  fields: (t) => ({
+    id: t.id({ nullable: false }),
+    userPermissions: t.field({ type: [PermissionRef] }),
+  }),
+})
